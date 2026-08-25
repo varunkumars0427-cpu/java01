d6c650081cddb05573eec0078cad6cd26d2555e6
varun kumar S <varunkumar.s0427@gmail.com>
2026-08-25 16:34:24 +0530

    Add files via upload

--- array.java
+++ array.java
@@ -1,36 +1,22 @@
-package day_5;
-import java.util.*;
+package day06;
+
 public class array {
-	public  static void main(String[] args) {
-		    HashSet set = new HashSet();
-		    System.out.println("Initial size of HashSet before adding elements: " + set.size());
-		    System.out.println("HashSet is empty before adding values: " + set.isEmpty());
-		    System.out.println("Contents of HashSet before adding values: " + set);
+	    public static void main(String[] args) {
+	        int[] numbers = {3, 1, 2, 4};
+	        int left = 0;
+	        int right = numbers.length - 1;
 
-		    set.add("C");
-		    set.add("A");
-		    set.add("E");
-		    set.add("B");
-		    set.add("D");
-		    set.add("F");
-		    set.add(null);
+	        while (left < right) {
+	            int temp = numbers[left];
+	            numbers[left] = numbers[right];
+	            numbers[right] = temp;
+	            left++;
+	            right--;
+	        }
 
-		    System.out.println("Size of HashSet after adding elements: " + set.size());
-		    System.out.println("HashSet is empty after adding values: " + set.isEmpty());
-	    	 System.out.println("Contents of HashSet after adding values: " + set);
-		 
-		 boolean b1 = set.contains("E");
-		 System.out.println("Value E is present: " + b1);
-		 boolean b2 = set.contains("R");
-		 System.out.println("Value R is present: " + b2);
-		 
-		 set.remove("B");
-		 System.out.println("Size of HashSet after deletion: " + set.size());
-		 System.out.println("Contents of HashSet after deletion: " + set);
-		
-		 set.clear();
-		 System.out.println("Size of HashSet after clearing: " + set.size());
-		
+	        System.out.println("Rearranged array:");
+	        for (int i = 0; i < numbers.length; i++) {
+	            System.out.print(numbers[i] + " ");
+	        }
+	    }
 	}
-
-}
