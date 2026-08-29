1b2c6bd1002ea4786dce229cc05d13943a0cf6a5
varun kumar S <varunkumar.s0427@gmail.com>
2026-08-29 16:01:53 +0530

    Add files via upload

---
+++ day10/tree_preorder.java
@@ -0,0 +1,31 @@
+package day10;
+
+public class tree_preorder {
+	
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data = data;
+		}
+	}
+	
+	static void preorder(Node root) {
+		if(root == null) {
+			return;
+		}
+		
+		System.out.println(root.data + " ");
+		preorder(root.left);
+		preorder(root.right);
+	}
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		preorder(root);
+	}
+}
